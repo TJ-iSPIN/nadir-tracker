4281a532dd3bb9b0a70a6a7f3d514af93d12fdda
AnikethL <102996935+AnikethL@users.noreply.github.com>
2022-05-11 15:35:58 -0400

    Update main.py

--- main.py
+++ main.py
@@ -4,4 +4,11 @@ from blurFile import blurImg
 blurVar=blurImg("earth2.jpeg")
 blurVar.blur()
 blurredImg = circleCenterInView("blurred.jpeg")
-blurredImg.approximater()
+x=0
+y=0
+for i in range(0,100):
+    x+=blurredImg.approximater()[0]
+    y+=blurredImg.approximater()[1]
+print("inview:",blurredImg.inView())
+x,y = (int(x/100),int(y/100))
+print(x,y)
